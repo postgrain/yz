d71ecf715e97f9c48e706b8f51bbd55ba92ede1e
Felipe Maia Vieira <fmaiavieira@gmail.com>
2022-07-01 15:27:02 -0300

    install ngrx

--- src/app/dice.ts
+++ src/app/dice.ts
@@ -1,13 +1,23 @@
 import { Roller } from './roller';
+import { createAction, props, Store } from '@ngrx/store';
+
+export const pipChange = createAction(
+  'pip change',
+  props<{ die: number; pips: number }>()
+);
 
 export class Dice {
   static UNKNOWN = 0;
   pips = [Dice.UNKNOWN, Dice.UNKNOWN, Dice.UNKNOWN, Dice.UNKNOWN, Dice.UNKNOWN];
 
-  constructor(private roller: Roller) {}
+  constructor(private roller: Roller, private store$: Store<any>) {}
 
   roll() {
-    this.pips = this.pips.map(() => this.roller.roll());
+    this.pips = this.pips.map((value, die) => {
+      const pips = this.roller.roll();
+      this.store$.dispatch(pipChange({ die, pips }));
+      return pips;
+    });
   }
 
   reset() {
