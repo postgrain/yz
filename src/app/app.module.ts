b100e9306491d2e5a769accde91269a883c0d6cf
chumarrento <lucasp28.contato@gmail.com>
2022-08-12 15:31:25 -0300

    score view wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,22 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { App } from './app';
 import { AppComponent } from './app.component';
+import { ScoresComponent } from './scores/scores.component';
+import { PlayersComponent } from './players/players.component';
+import { PlayerComponent } from './player/player.component';
+import { DescriptionComponent } from './description/description.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ScoresComponent, PlayersComponent, PlayerComponent, DescriptionComponent],
   imports: [
     BrowserModule,
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument({}),
+  ],
+  exports: [
+    ScoresComponent,
+    AppComponent
   ]
 })
 export class AppModule implements DoBootstrap{
--- src/making-app/making-app.module.ts
+++ src/making-app/making-app.module.ts
@@ -13,11 +13,12 @@ import { MakingAppComponent } from "./making-app.component";
   })
   export class MakingAppModule implements DoBootstrap {
     constructor(public appModule: AppModule) {}
-  
+
     ngDoBootstrap(app: ApplicationRef) {
-      this.appModule.ngDoBootstrap(app);
+      // this.appModule.ngDoBootstrap(app);
       const tag =  document.createElement('app-making')
-      document.body.prepend(tag);
+      document.body.querySelector('app-root')?.remove()
+      document.body.append(tag);
       app.bootstrap(MakingAppComponent);
     }
   }
